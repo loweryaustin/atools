6e0e8c74e3766bc65f0992ccd478d4520d9a9beb
Austin Lowery <loweryaustin@gmail.com>
2018-12-12 20:08:29 -0600

    Add bkup log #44

--- perl/bkup.pl
+++ perl/bkup.pl
@@ -1,12 +1,40 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
-use File::Copy qw(move);
+use File::Copy qw(cp);
+use Cwd qw(cwd);
+use POSIX qw(strftime);
+
+ 
+## Set Options
+
+## Handle Input
+my $fileInput = shift or die "Usage: $0 FILENAME\n";
+
+# TODO: input validataion
+# TODO: allow for the use of full paths
+
+## Setup
+my $date = strftime "%F %H:%M:%S", localtime;
+
+my $user = $ENV{LOGNAME} || $ENV{USER} || getpwuid($<);
+my $homeDir = $ENV{HOME} || (getpwuid $<)[7];
 
 
-my $filename = shift or die "Usage: $0 FILENAME\n";
-my $backupDir = '/root/bkups/';
+my $backupDir = $homeDir."/bkups/";
+my $logPath = $backupDir.'bkup.log';
 
+my $cwd = cwd;
+my $origPath = $cwd.$fileInput;
+my $destPath = $backupDir.$fileInput;
+print $backupDir;
 mkdir ($backupDir, 0755);
+open my $fhout, ">", $logPath or die $!;
 
-move $filename, $backupDir.$filename;
+
+## Do the thing
+cp $fileInput, $destPath;
+select $fhout;
+print $fhout $date." ".$user." ".$origPath." -> ".$destPath;
+select STDOUT;
+print "Copied ".$origPath." -> ".$destPath;
