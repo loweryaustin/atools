f2e79b3734d0854fd89c54587f97eb5a01fe4d63
Austin Lowery <loweryaustin@gmail.com>
2018-12-20 23:38:47 -0600

    Add input validation #50 & Fullpath support #49

--- bkup.pl
+++ bkup.pl
@@ -2,45 +2,63 @@
 use strict;
 use warnings;
 use File::Copy qw(cp);
-use Cwd qw(cwd);
+use File::Spec::Functions;
+use File::Spec;
 use POSIX qw(strftime);
 
 ###################################
-# BKUP - Version 0.5.0            #
+# BKUP - Version 0.6.0            #
 #                                 #
 # Released under the MIT License  #
 # By Austin Lowery                #
 ###################################
 
-## Set Options
+
 
 ## Handle Input
-my $fileInput = shift or die "Usage: $0 FILENAME\n";
+my $input = shift or die "Usage: $0 FILENAME\n";
 
-# TODO: input validataion
-# TODO: allow for the use of full paths
+my $origPath = validateFilePath($input);
+my ($volume, $dirs, $fileName) = File::Spec->splitpath($origPath);
 
 ## Setup
+
 my $date = strftime "%F %H:%M:%S", localtime;
 my $epoch = time();
-
 my $user = $ENV{LOGNAME} || $ENV{USER} || getpwuid($<);
 my $homeDir = $ENV{HOME} || (getpwuid $<)[7];
-
-
 my $backupDir = $homeDir."/bkups/";
 my $logPath = $backupDir.'bkup.log';
+my $destPath = $backupDir.$fileName."-".$epoch;
 
-my $cwd = cwd."/";
-my $origPath = $cwd.$fileInput;
-my $destPath = $backupDir.$fileInput."-".$epoch;
-mkdir ($backupDir, 0755);
 open my $fhout, ">>", $logPath or die $!;
 
-
 ## Do the thing
-cp $fileInput, $destPath;
+mkdir ($backupDir, 0755);
+cp $origPath, $destPath;
 select $fhout;
 print $fhout $date." ".$user." ".$origPath." -> ".$destPath."\n";
 select STDOUT;
 print "Copied ".$origPath." -> ".$destPath;
+
+## Subs
+
+sub validateFilePath {
+
+	my ($input) = @_;
+	
+	if (! -e $input) {
+
+		die ("$input not found.");
+	} elsif (-d $input){
+
+		die ("$input is a directory. Directory support is planed in feature #48. Check pm.austinlowery.com for more info.");	
+	} elsif (! file_name_is_absolute($input)) {
+		
+		return File::Spec->rel2abs($input);
+	} else {
+		
+		return $input
+	}	
+}
+
