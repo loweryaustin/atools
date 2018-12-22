4ac692bc5c870acd4ef88293ebfab86d22e8c7ac
Austin Lowery <loweryaustin@gmail.com>
2018-12-22 01:00:23 -0600

    Squashed commit of the following:
    commit 8f8d5e5dc312005367b3cff2591e5192d51b8a74
    Author: Austin Lowery <loweryaustin@gmail.com>
    Date:   Sat Dec 22 00:58:56 2018 -0600
    
        Fixup for 0.7.0 release.
    
    commit 1f5610bdd66653d71c18948f5df82440de9512a3
    Author: Austin Lowery <loweryaustin@gmail.com>
    Date:   Sat Dec 22 00:54:40 2018 -0600
    
        Add file cleanup for installation.
    
    commit 0ad7e7f9501b8e79447dd1118d8893d2265f748b
    Author: Austin Lowery <loweryaustin@gmail.com>
    Date:   Sat Dec 22 00:53:23 2018 -0600
    
        Minor whitespace fixes.
    
    commit 2cf02cb461b1c2339887b8ab7f87d78b1094f836
    Author: Austin Lowery <loweryaustin@gmail.com>
    Date:   Sat Dec 22 00:51:48 2018 -0600
    
        Fix placement of option variable.
    
    commit da88c5ebf0defd671a63eb2d0679dd68d9fc4836
    Author: Austin Lowery <loweryaustin@gmail.com>
    Date:   Sat Dec 22 00:50:18 2018 -0600
    
        Minor case fix
    
    commit 980b0d2140a689f57a65736d64694ce4feedb7a2
    Author: Austin Lowery <loweryaustin@gmail.com>
    Date:   Sat Dec 22 00:49:03 2018 -0600
    
        Add installation instructions
    
    commit 2fc6564d27592396b65335ad5aa55e32ce029f76
    Author: Austin Lowery <loweryaustin@gmail.com>
    Date:   Sat Dec 22 00:14:59 2018 -0600
    
        Add one command install option.
    
    commit 8ed0ba2f002e59d29cdfe2c768374e4dc65115b1
    Author: Austin Lowery <loweryaustin@gmail.com>
    Date:   Fri Dec 21 13:36:24 2018 -0600
    
        Add command line options

---
+++ README.md
@@ -0,0 +1,8 @@
+#bkup
+
+A file backup utility.
+
+## Installation
+```
+if [ $(md5sum bkup.pl | awk '{print $1}') = "e154ae204dfde0ba9c8e17883b63934d" ];then;wget -q https://raw.githubusercontent.com/loweryaustin/bkup/one-command-install-91/bkup.pl; perl bkup.pl --install;else echo "MD5SUM does not match. Software may have been tampered with. Installation cancelled.";fi
+```
--- bkup.pl
+++ bkup.pl
@@ -2,30 +2,48 @@
 use strict;
 use warnings;
 use File::Copy qw(cp);
-use File::Spec::Functions;
+use File::Copy qw(move);
 use POSIX qw(strftime);
+use File::Spec::Functions;
+use File::Path qw(make_path);
+use Getopt::Long qw(GetOptions);
+Getopt::Long::Configure qw(gnu_getopt);
 
 ###################################
-# BKUP - Version 0.6.1            #
+# bkup - Version 0.7.0            #
 #                                 #
 # Released under the MIT License  #
 # By Austin Lowery                #
 ###################################
-
-## Handle Input
-my $input = shift or die "Usage: $0 FILENAME\n";
-my $origPath = validateFilePath($input);
-my ($volume, $dirs, $fileName) = File::Spec->splitpath($origPath);
 
 ## Setup
 my $date = strftime "%F %H:%M:%S", localtime;
 my $epoch = time();
 my $user = $ENV{LOGNAME} || $ENV{USER} || getpwuid($<);
 my $homeDir = $ENV{HOME} || (getpwuid $<)[7];
+my $defaultFallbackInstallDir = "$homeDir/bkups/bin/";
 my $backupDir = $homeDir."/bkups/";
 my $logPath = $backupDir.'bkup.log';
-my $destPath = $backupDir.$fileName."-".$epoch;
 open my $fhout, ">>", $logPath or die $!;
+
+## Options
+my $install;
+my $installDir = "/usr/local/bin/";
+
+## Handle Input
+GetOptions(
+    'install|i' => \$install,
+    'install-dir=s' => \$installDir,
+) or die "No help built in. You're on your own for now.\n";
+
+if ($install){
+	installBkup();	
+}
+
+my $input = shift or die "Usage: $0 FILENAME\n";
+my $origPath = validateFilePath($input);
+my ($volume, $dirs, $fileName) = File::Spec->splitpath($origPath);
+my $destPath = $backupDir.$fileName."-".$epoch;
 
 ## Do the thing
 mkdir ($backupDir, 0755);
@@ -37,21 +55,28 @@ print "Copied ".$origPath." -> ".$destPath;
 
 ## Subs
 sub validateFilePath {
-
 	my ($input) = @_;
-	
 	if (! -e $input) {
-
 		die ("$input not found.");
 	} elsif (-d $input){
-
 		die ("$input is a directory. Directory support is planed in feature #48. Check pm.austinlowery.com for more info.");	
 	} elsif (! file_name_is_absolute($input)) {
-		
 		return File::Spec->rel2abs($input);
 	} else {
-		
 		return $input
 	}	
 }
 
+sub installBkup {
+	if ($installDir eq $defaultFallbackInstallDir) { make_path($installDir); }; # Since this is the default fallback directroy, we'll create it to faciliate the quick and easy one step installation process that this script must posess.
+	if (! -e $installDir) { die("$installDir does not exist. Please create it or choose a different directory")};
+	if (! -d $installDir) { die("$installDir is not a directory. You must use a directory with the --install-dir flag.")};
+	if (! -w $installDir) { die("You do not have permission to install bkup to $installDir . Try using sudo. If you don't have sudo access, you can use --install-dir [Path to install Dir] in additon to the install flag to specify a directory that you have permission to write to.\n");};
+	my $installDest = $installDir."bkup"; 
+	if (-e $installDest) { die("bkup is already installed at $installDest\n");};
+	cp ($0, $installDest);
+	chmod 0755, $installDest;
+	unlink $0;
+	print "Installed to $installDest";
+	exit;
+}
